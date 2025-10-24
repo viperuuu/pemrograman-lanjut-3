77336f43b5c311f80a3448a15de543ca6bfb6fc4
fawwaz <icamrz8@gmail.com>
2025-10-24 16:04:04 +0700

    init comit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("masukan nama lengkap anda: ");
+        String nama = scanner.nextLine();
+
+        System.out.print("masukan umur anda: ");
+        String umur = scanner.nextLine();
+
+
+        System.out.print("masukan nilai anda: ");
+        String nilai = scanner.nextLine();
+
+
+        try {
+            int nilaiInt = Integer.parseInt(nilai.trim());
+            System.out.println("halo nama saya: " + nama);
+            System.out.println("umur saya: " + umur);
+
+            if (nilaiInt >= 75) {
+                System.out.println("selamat anda lulus");
+            } else {
+                System.out.println("maaf anda tidak lulus");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Nilai tidak valid — masukkan angka yang benar.");
+        }
+        scanner.close();
+    }
+
+
+}
